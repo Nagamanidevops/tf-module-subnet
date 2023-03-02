82cf414a520f8d3c2e68ec4edf0b79527610cd44
nagamani <nagamanichavaaws@gmail.com>
2023-03-02 07:41:01 +0000

    v

--- main.tf
+++ main.tf
@@ -29,4 +29,10 @@ resource "aws_route_table" "route_table" {
 }
 
 
+resource "aws_route_table_association" "assossiation" {
+count = length(aws_subnet.main)
+  subnet_id      = aws_subnet.main.*.id[count.index]
+  route_table_id = aws_route_table.route_table.id
+}
+
   
